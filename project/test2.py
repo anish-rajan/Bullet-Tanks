2efb06ce09b057d7a11428c78a5f4e8440ae97ba
AnishRaj2000 <anishrajan2000@gmail.com>
2018-11-28 09:58:55 -0500

    New Changes

--- project/test2.py
+++ project/test2.py
@@ -2,32 +2,43 @@ import pygame,sys
 if __name__=="__main__":
     pygame.init()
     pygame.font.init()
+class Character:
+    def __init__(self,left,right,stand,weapon):
+        self.left=left
+        self.right=right
+        self.stand=stand
+        self.state=stand
+        self.weapon=weapon
 
 myfont=pygame.font.SysFont('Comic Sans MS',200)
 myfonto=pygame.font.SysFont('Comic Sans MS',100)
 myfontl=pygame.font.SysFont('Comic Sans MS',30)
 size = width, height = 1800, 1000
 black = (0, 0, 0)
-s=-1
+
 screen = pygame.display.set_mode(size)
+#Fonts
 Menu=myfont.render("MENU",False,(255,0,0))
 Start=myfonto.render("START",False,(255,0,0))
 Quit=myfonto.render("QUIT",False,(255,0,0))
+#Menu background
 menubg=pygame.image.load("menu.png")
 menubg=pygame.transform.scale(menubg,(1800,1000))
-ball= pygame.image.load("captain.png")
+#Captain stationary is caps
+caps= pygame.image.load("cap/captain.png")
+#Road
 road=pygame.image.load("road.png")
 road=pygame.transform.scale(road,(1800,125))
 roadrect=road.get_rect()
-ball= pygame.transform.scale(ball,(125,125))
-cap=ball
-ballrect=cap.get_rect()
-capr=[pygame.image.load("capr1.png"),pygame.image.load("capr2.png"),pygame.image.load("capr3.png"),pygame.image.load("capr4.png"),pygame.image.load("capr5.png"),pygame.image.load("capr6.png")]
-capl=[pygame.image.load("capl1.png"),pygame.image.load("capl2.png"),pygame.image.load("capl3.png"),pygame.image.load("capl4.png"),pygame.image.load("capl5.png"),pygame.image.load("capl6.png")]
+caps= pygame.transform.scale(caps,(125,125))
+#Cap is the state of the characters
+cap=caps
+capr=[pygame.image.load("cap/capr1.png"),pygame.image.load("cap/capr2.png"),pygame.image.load("cap/capr3.png"),pygame.image.load("cap/capr4.png"),pygame.image.load("cap/capr5.png"),pygame.image.load("cap/capr6.png")]
+capl=[pygame.image.load("cap/capl1.png"),pygame.image.load("cap/capl2.png"),pygame.image.load("cap/capl3.png"),pygame.image.load("cap/capl4.png"),pygame.image.load("cap/capl5.png"),pygame.image.load("cap/capl6.png")]
 for i in range(6):
     capr[i]=pygame.transform.scale(capr[i],(125,125))
     capl[i]=pygame.transform.scale(capl[i],(125,125))
-shield=pygame.image.load("shield.png")
+shield=pygame.image.load("cap/shield.png")
 shield=pygame.transform.scale(shield,(90,90))
 shieldrect=shield.get_rect()
 tank=pygame.image.load("tank.png")
@@ -57,7 +68,9 @@ levelc=1
 speed=1
 
 pygame.display.set_caption("Bullet Tanks")
+#Start Of the game loop
 while 1:
+    #Start of menu
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
@@ -77,7 +90,23 @@ while 1:
         screen.blit(Start,(800,450))
         screen.blit(Quit,(830,550))
         pygame.display.update()
-
+    #Character Choosing
+    '''
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+        keys=pygame.key.get_pressed()
+        r=0
+        l=0
+        caps=pygame.transform.scale(caps,(400,400))
+        if keys[pygame.K_RIGHT]:
+            r=1
+        if keys[pygame.K_LEFT]:
+            l=1
+        screen.blit(menubg,(0,0))
+        screen.blit(caps,(750,300))
+        pygame.display.update()
+'''
     k=0
     x=0
     y=0
@@ -94,7 +123,8 @@ while 1:
     count=10
     level=1
     speed=1
-
+    caps=pygame.transform.scale(caps,(125,125))
+#main game
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
@@ -104,17 +134,17 @@ while 1:
         vx=0
         vy=0
         keys=pygame.key.get_pressed()
-
+#Controls of the game
         if event.type==pygame.KEYDOWN:
             if event.key == pygame.K_UP and y>0:
                 y-=10
                 vy=-10
-                cap=ball
+                cap=caps
                 l=0
             if event.key == pygame.K_DOWN and (y+125)<height:
                 y+=10
                 vy=10
-                cap=ball
+                cap=caps
                 l=0
             if event.key == pygame.K_LEFT and x>0:
                 x-=5
@@ -165,9 +195,12 @@ while 1:
                 else:
                     count+=1
 
-        if k==0 and shieldrect[0]==x:
+        if k==0 and shieldrect[0]==x:                #Shield Controls
             if keys[pygame.K_SPACE]:
                 k=1
+#Controls end
+
+#Shield Movement
         if k==1:
             shieldrect=shieldrect.move([10,0])
             if shieldrect[0]>1800-125:
@@ -183,15 +216,16 @@ while 1:
             if m[i] and t[i]:
                 s[i][0]-=speed
 
+#Road and tank Display
         for i in range(0,1000,125):
             screen.blit(road,(0,i))
-            if t[i/125]<1:
+            if t[i/125]<1:               # t is the life of the tank.
                 continue
             screen.blit(tank,(1800-125,i))
 
         if k==1:
             screen.blit(shield,shieldrect)
-
+# Collision of shield with bullet and tanks
         if not (s[u][0]>=x-10 and s[u][0]<=x+10):
             for i in range(8):
                 if m[i]:
@@ -205,18 +239,20 @@ while 1:
                     if s[i][0]<10:
                         m[i]=False
                         s[i][0]=1800-125
+
+#Collision of bullet with man
         else:
             lives-=1
             s[u][0]=1800-125
             m[u]=False
-
+#CAPTAIN AMERICA DISPLAY
         if lives>0:
             screen.blit(cap,(x,y))
         else:
             break
         Lives=myfontl.render("Lives:"+str(lives),False,(255,255,255))
 
-
+#Levels display
         screen.blit(Lives,(0,0))
         if levelc>=0:
             screen.blit(levels,(700,450))
@@ -225,6 +261,7 @@ while 1:
 
         if level==5 and 1 not in t:
             break
+#Reinitialising of the variables for next level
         elif 1 not in t:
             level+=1
             screen.blit(levels,(700,450))
